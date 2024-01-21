02eb0e07c06d967f5ea88221b4dbb9f118d05b8e
keyzeroc <gozveruga@gmail.com>
2024-01-21 15:16:10 +0200

    ...v2

--- src/components/UI/Nav.jsx
+++ src/components/UI/Nav.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Nav() {
   return (
     <nav>
-      <ul className="p-16 list-none flex flex-row justify-between items-baseline text-white font-light text-5xl">
-        <li className="">Nikke Advise</li>
+      <ul className="p-16 list-none flex flex-row justify-between items-baseline text-selector font-light text-5xl">
+        <li>Nikke Advise</li>
       </ul>
     </nav>
   );
